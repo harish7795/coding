b58772de996ed6a5635855760bf29fdee41f714c
harishhm7795 <harishhm779@gmail.com>
2022-12-19 18:59:04 +0530

    add the new file

---
+++ dom.js
@@ -0,0 +1,18 @@
+var count=0;
+function myFunction(){
+    count+=1
+    // console.log(count);
+    alert(count)
+    console.log(alert);
+
+
+}
+
+var accessHeadind=document.getElementById("heading").innerText
+console.log(accessHeadind);
+
+var para=document.getElementById("suma").innerText
+console.log(para);
+
+var movies=document.getElementById("chinna").innerText
+console.log(movies);
